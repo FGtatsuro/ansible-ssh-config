97072000ca7d3a439060d7995371483eb97db2ef
FGtatsuro <fujiistorage@gmail.com>
2016-07-17 22:08:08 +0900

    OSX on Travis(travis user) knows github.com/bitbucket.org in default, but /Users/travis/.ssh/known_hosts doesn't include these string.

--- spec/ssh_config_spec.rb
+++ spec/ssh_config_spec.rb
@@ -10,6 +10,10 @@ describe file("#{ENV['SSHCONFIG_HOME']}/.ssh/known_hosts") do
   it { should be_file }
   it { should be_owned_by ENV['SSHCONFIG_OWNER'] }
   it { should be_grouped_into ENV['SSHCONFIG_GROUP'] }
+end
+
+# Avoid OSX on Travis.
+describe file("#{ENV['SSHCONFIG_HOME']}/.ssh/known_hosts"), :if => os[:family] == 'debian' do
   its(:content) { should include('github.com') }
   its(:content) { should include('bitbucket.org') }
 end
