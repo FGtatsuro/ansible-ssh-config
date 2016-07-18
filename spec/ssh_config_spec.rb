dcc0e29651459534c3cbf1d284cc3ac8c0423d3d
FGtatsuro <fujiistorage@gmail.com>
2016-07-19 00:59:36 +0900

    Add specs related to authorized keys.

--- spec/ssh_config_spec.rb
+++ spec/ssh_config_spec.rb
@@ -30,6 +30,17 @@ end
   end
 end
 
+describe file("#{ENV['SSHCONFIG_HOME']}/.ssh/authorized_keys") do
+  it { should be_file }
+  it { should be_owned_by ENV['SSHCONFIG_OWNER'] }
+  it { should be_grouped_into ENV['SSHCONFIG_GROUP'] }
+  it { should be_mode 600 }
+  # id_rsa1.pub
+  its(:content) { should include('AAAAB3NzaC1yc2EAAAADAQABAAABAQDHz9qJRRY9KNxxBc+FxYz904GrtCtDYTHxeBY1NPZ2RtNQQOyGZIr4XFDIv89rG017rJh3xIcYXixBJRN/GQq8GexjJBq8uB7OO65smeh7w5iZe7++2672eR8zpWmDkymLzWDuRR3c6Mx+/9jSiYNy0+4mdghYLYdMqjmbLDMWa7wOWVVRgEGhWDVJ7MP6AWydICB5V+yaIlLu4cdmS7NUXRPrrHAmAph3lsvG1P9oovBTh2KBdaGqfMlYTiwQhy/e+5WDZY9/AQ+zOAfuDMdiKHiLm9+3L/16SaT9im+tW/a0BKAuIYb8qe7b60QkeUY/i9QFla4fsC3tl4D+KN8f') }
+  # id_rsa2.pub
+  its(:content) { should include('AAAAB3NzaC1yc2EAAAADAQABAAABAQC1iecz1Lorz1FQS8EpgRccf9SXJEPsLyDBFUH32aqIyylvV1PC5iWNcHTWs03fFLV63QeI5PX1Bl7CAG0Gea2xmsaeJWU6esqVbbq5wgkMlErtX9CeQyjQGJ98rKc/fSHZua2iCRwhfijyJGMKcH36K5bcE/yGJwiiLc+1igUMxNb7qP1eV+874+3oIP51y1ZWw8Q4nGh3jkQ4cTG08TX32ucrLu3uqWOnHokXuTxoTh9UdvLopKnGVvUVroqeDZXaLtFSaWG9vygaFAYO7I7sQii8OqEf77ra0CupkUvK2ft0FGWZxoGMEMdJX56cGC0YyZhFhyE6MgviX6e7A783') }
+end
+
 # Avoid OSX on Travis.
 describe file("#{ENV['SSHCONFIG_HOME']}/.ssh/known_hosts"), :if => os[:family] == 'debian' do
   its(:content) { should include('github.com') }
